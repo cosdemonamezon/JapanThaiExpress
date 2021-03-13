8b9c9cdf45be881cba2c54f6fc2cddb81d75dd8d
thawatchai_m <demonridercos@gmail.com>
2021-03-13 23:13:16 +0700

    aa

--- lib/UserScreens/Service/Deposit.dart
+++ lib/UserScreens/Service/Deposit.dart
@@ -1,6 +1,9 @@
+import 'dart:io' as Io;
+import 'dart:convert';
 import 'package:JapanThaiExpress/UserScreens/Service/DetailStep.dart';
 import 'package:JapanThaiExpress/UserScreens/WidgetsUser/NavigationBar.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Deposit extends StatefulWidget {
   Deposit({Key key}) : super(key: key);
@@ -12,6 +15,8 @@ class Deposit extends StatefulWidget {
 class _DepositState extends State<Deposit> {
   List<bool> checked = [false, true, false, false, true];
   String valueChoose;
+  Io.File _image;
+  final picker = ImagePicker();
   List dropdownValue = [
     "Food",
     "Transport",
@@ -22,211 +27,236 @@ class _DepositState extends State<Deposit> {
     "Movie",
     "Salary"
   ];
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = Io.File(pickedFile.path);
+        final bytes = Io.File(pickedFile.path).readAsBytesSync();
+        // String base64Encode(List<int> bytes) => base64.encode(bytes);
+        String img64 = base64Encode(bytes);
+        print(img64);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          centerTitle: true,
-          title: Text("รับฝากส่ง"),
-          bottom: TabBar(
-            labelColor: Colors.redAccent,
-            unselectedLabelColor: Colors.white,
-            indicatorSize: TabBarIndicatorSize.label,
-            indicator: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10)),
-              color: Colors.white),
-            tabs: [
-              Tab(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text("ฝากส่ง"),
-                ),
-              ),
-              Tab(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text("ประวัติ"),
-                ),
-              ),  
-            ]
-          )
-        ),
-        body: TabBarView(
-          children: [
-            Container(
-              height: height,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: height * .04),
-                    
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("ที่อยู่", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            maxLines: 4,
-                            decoration: InputDecoration(
-                            //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true
-                            ),
-                          ),
-                        ],
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+              elevation: 0,
+              centerTitle: true,
+              title: Text("รับฝากส่ง"),
+              bottom: TabBar(
+                  labelColor: Colors.redAccent,
+                  unselectedLabelColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10)),
+                      color: Colors.white),
+                  tabs: [
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("ฝากส่ง"),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("รายละเเอียด", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            decoration: InputDecoration(
-                            //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true
-                            ),
-                          ),
-                        ],
+                    Tab(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("ประวัติ"),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("อัปโหลดรูป", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            decoration: InputDecoration(
-                            //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true,
-                              suffixIcon: Icon(
-                                Icons.file_upload,
-                                size: 25,
-                              ),
+                  ])),
+          body: TabBarView(
+            children: [
+              Container(
+                height: height,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: height * .04),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "ที่อยู่",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 10),
+                            TextFormField(
+                              maxLines: 4,
+                              decoration: InputDecoration(
+                                  //border: InputBorder.none,
+                                  border: OutlineInputBorder(),
+                                  fillColor: Color(0xfff3f3f4),
+                                  filled: true),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [                          
-                          GestureDetector(
-                            onTap: (){
-                              Navigator.push(
-                                context, MaterialPageRoute(builder: (context) => DetailStep())
-                              );
-                            },
-                            child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              padding: EdgeInsets.symmetric(vertical: 15),
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(5)),
-                                boxShadow: <BoxShadow>[
-                                  BoxShadow(
-                                      color: Colors.grey.shade200,
-                                      offset: Offset(2, 4),
-                                      blurRadius: 5,
-                                      spreadRadius: 2)
-                                ],
-                                gradient: LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.centerRight,
-                                  colors: [Color(0xffdd4b39), Color(0xffdd4b39)]
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "รายละเอียด",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  //border: InputBorder.none,
+                                  border: OutlineInputBorder(),
+                                  fillColor: Color(0xfff3f3f4),
+                                  filled: true),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          getImage();
+                        },
+                        child: Center(
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            //color: Colors.red,
+                            child: _image == null
+                                ? Image.asset("assets/images/card_bg.png")
+                                : Image.file(
+                                    _image,
+                                    fit: BoxFit.cover,
+                                  ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => DetailStep()));
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.symmetric(vertical: 15),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                  boxShadow: <BoxShadow>[
+                                    BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: Offset(2, 4),
+                                        blurRadius: 5,
+                                        spreadRadius: 2)
+                                  ],
+                                  gradient: LinearGradient(
+                                      begin: Alignment.centerLeft,
+                                      end: Alignment.centerRight,
+                                      colors: [
+                                        Color(0xffdd4b39),
+                                        Color(0xffdd4b39)
+                                      ]),
+                                ),
+                                child: Text(
+                                  "Confirm",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
                                 ),
                               ),
-                              child: Text("Confirm", style: TextStyle(fontSize: 20, color: Colors.white),),
                             ),
-                          ),
-                          SizedBox(height: 15),
-                        ],
+                            SizedBox(height: 15),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-
-            //Tab ที่สอง
-            Column(
-              children: [
-                buildCard(
-                  "Hi everyone in this flutter article I am working with flutter button UI Design. Flutter button with image",
-                  "assets/o8.jpg",
-                ),
-                buildCard(
-                  "Buttons are the Flutter widgets, which is a part of the material design library. Flutter provides several types of buttons that have different shapes",
-                  "assets/o7.jpg",
-                ),
-              ],
-            ),
-            
-
-          ],
-          
-        ),
-        bottomNavigationBar: NavigationBar(),
-      )
-    );
+              //Tab ที่สอง
+              Column(
+                children: [
+                  buildCard(
+                    "Hi everyone in this flutter article I am working with flutter button UI Design. Flutter button with image",
+                    "assets/o8.jpg",
+                  ),
+                  buildCard(
+                    "Buttons are the Flutter widgets, which is a part of the material design library. Flutter provides several types of buttons that have different shapes",
+                    "assets/o7.jpg",
+                  ),
+                ],
+              ),
+            ],
+          ),
+          bottomNavigationBar: NavigationBar(),
+        ));
   }
 
   Card buildCard(String title, String image) {
     return Card(
       child: ListTile(
-        leading: CircleAvatar(
-          radius: 25,
-          backgroundImage: AssetImage(image),
-        ),
-        title: Text(
-          title,
-          style: TextStyle(
-            fontWeight: FontWeight.bold, color: Colors.black, fontSize: 14,
+          leading: CircleAvatar(
+            radius: 25,
+            backgroundImage: AssetImage(image),
           ),
-        ),
-        subtitle: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            MaterialButton(
-              onPressed: (){},
-              color: Colors.green,
-              child: Text(
-                "Details",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, color: Colors.white, fontSize: 12,
+          title: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 14,
+            ),
+          ),
+          subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.green,
+                child: Text(
+                  "Details",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
                 ),
               ),
-            ),
-          ],
-        )
-      ),
+            ],
+          )),
     );
   }
-
-
-
 }
