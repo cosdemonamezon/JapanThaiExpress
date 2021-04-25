4d8cbe2d656c88faf184ca6c9db32e93db6018cb
BLackBodyX64 <anonnon3952@gmail.com>
2021-04-26 00:32:58 +0700

    สมัครสมาชิก

--- lib/Screens/Register/Registration.dart
+++ lib/Screens/Register/Registration.dart
@@ -1,8 +1,8 @@
 import 'package:JapanThaiExpress/Screens/Register/OtpScreen.dart';
-import 'package:JapanThaiExpress/Screens/Register/SetPin.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class Registration extends StatefulWidget {
   String name, lastname, password, repassword, email;
   Registration({Key key}) : super(key: key);
@@ -11,19 +11,15 @@ class Registration extends StatefulWidget {
   _RegistrationState createState() => _RegistrationState();
 }
 
-//final _formKey = GlobalKey<FormState>();
-
 class _RegistrationState extends State<Registration> {
   final _formKey = GlobalKey<FormBuilderState>();
-  static final GlobalKey<FormFieldState<String>> _searchFormKey =
-      GlobalKey<FormFieldState<String>>();
+
   @override
   Widget build(BuildContext context) {
-    String _name, _lastname, _password, _repassword, _email;
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Registration"),
+        title: Text("สมัครสมาชิก"),
       ),
       body: Container(
         height: height,
@@ -52,7 +48,7 @@ class _RegistrationState extends State<Registration> {
                             name: 'name',
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.person),
-                                labelText: 'Fullname',
+                                labelText: 'ชื่อ',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -71,7 +67,7 @@ class _RegistrationState extends State<Registration> {
                             name: 'lname',
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.person),
-                                labelText: 'Lastname',
+                                labelText: 'นามสกุล',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -91,7 +87,7 @@ class _RegistrationState extends State<Registration> {
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.email),
-                                labelText: 'Email',
+                                labelText: 'อีเมล',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -111,7 +107,7 @@ class _RegistrationState extends State<Registration> {
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.phone),
-                                labelText: 'Tel',
+                                labelText: 'หมายเลขโทรศัพท์',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -132,7 +128,7 @@ class _RegistrationState extends State<Registration> {
                             obscuringCharacter: "•",
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.lock),
-                                labelText: 'Password',
+                                labelText: 'รหัสผ่าน',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -153,7 +149,7 @@ class _RegistrationState extends State<Registration> {
                             obscuringCharacter: "•",
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.lock),
-                                labelText: 'Confirm Password',
+                                labelText: 'ยืนยันรหัสผ่าน',
                                 //border: InputBorder.none,
                                 border: OutlineInputBorder(),
                                 fillColor: Color(0xfff3f3f4),
@@ -165,243 +161,12 @@ class _RegistrationState extends State<Registration> {
                     ),
                   ),
                 ),
-                SizedBox(height: 5),
-                /*Container(
-                  margin: EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "นามสกุล",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.person),
-                              labelText: 'lastname',
-                              //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return "please enter your lastname";
-                            }
-                            return null;
-                          } ,
-                          onSaved: (String lastname){
-                          _lastname=lastname;
-                        },
-                        ),  
-                    ],
-                  ),
-                ),
-                SizedBox(height: 5),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "อีเมล์",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.email),
-                              labelText: 'email',
-                              //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return "please enter your E-mail";
-                            }
-                            ;
-                            return null;
-                          },
-                           onSaved: (String email){
-                          _email=email;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 5),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "เบอร์โทรศัพท์",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(height: 10),
-                       TextFormField(
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.phone),
-                              labelText: 'Tel',
-                              //border: InputBorder.none,
-                              border: OutlineInputBorder(),
-                              fillColor: Color(0xfff3f3f4),
-                              filled: true),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return "please enter your telephone number";
-                            }
-                            ;
-                            return null;
-                          }),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "รหัสผ่าน",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(height: 10),
-                      Form(child:
-                      TextFormField(
-                        obscureText: true,
-                        obscuringCharacter: "•",
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            labelText: 'Password',
-                            //border: InputBorder.none,
-                            border: OutlineInputBorder(),
-                            fillColor: Color(0xfff3f3f4),
-                            filled: true),
-                        validator: (String value) {
-                          if (value.isEmpty) {
-                            return 'กรุณากรอกรหัสผ่าน';
-                          }
-                          return null;
-                        },
-                        onSaved: (String password){
-                          _password=password;
-                        },
-                      ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "ยืนยันรหัสผ่าน",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                        obscureText: true,
-                        obscuringCharacter: "•",
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            labelText: 'Confirm Password',
-                            //border: InputBorder.none,
-                            border: OutlineInputBorder(),
-                            fillColor: Color(0xfff3f3f4),
-                            filled: true),
-                        validator: (String value) {
-                          
-                           if (value.isEmpty) {
-                            return 'กรุณายืนยันรหัสผ่าน';
-                          }
-                          if(password.text != repassword.text){
-                            return "Password Do not match";
-                          }
-                          return null;
-                        },
-                        onSaved: (String repassword){
-                          _repassword=repassword;
-                        },
-                      ),
-                    ],
-                  ),
-                ),*/
-                // Container(
-                //   margin: EdgeInsets.symmetric(vertical: 5),
-                //   child: Column(
-                //     crossAxisAlignment: CrossAxisAlignment.start,
-                //     children: [
-                //       Text("User", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                //       SizedBox(height: 10),
-                //       TextFormField(
-                //         decoration: InputDecoration(
-                //           //border: InputBorder.none,
-                //           border: OutlineInputBorder(),
-                //           fillColor: Color(0xfff3f3f4),
-                //           filled: true
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
-                // SizedBox(height: 5),
-                // Container(
-                //   margin: EdgeInsets.symmetric(vertical: 5),
-                //   child: Column(
-                //     crossAxisAlignment: CrossAxisAlignment.start,
-                //     children: [
-                //       Text("Password", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                //       SizedBox(height: 10),
-                //       TextFormField(
-                //         decoration: InputDecoration(
-                //           //border: InputBorder.none,
-                //           border: OutlineInputBorder(),
-                //           fillColor: Color(0xfff3f3f4),
-                //           filled: true
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
-                // SizedBox(height: 5),
-                // Container(
-                //   margin: EdgeInsets.symmetric(vertical: 5),
-                //   child: Column(
-                //     crossAxisAlignment: CrossAxisAlignment.start,
-                //     children: [
-                //       Text("Confirm Password", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                //       SizedBox(height: 10),
-                //       TextFormField(
-                //         decoration: InputDecoration(
-                //           //border: InputBorder.none,
-                //           border: OutlineInputBorder(),
-                //           fillColor: Color(0xfff3f3f4),
-                //           filled: true
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 GestureDetector(
                   onTap: () {
-
                     _formKey.currentState.save();
                     final isValid = _formKey.currentState.validate();
                     if (isValid) {
-                      
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
                         return OtpScreen();
@@ -427,12 +192,12 @@ class _RegistrationState extends State<Registration> {
                           colors: [Color(0xffdd4b39), Color(0xffdd4b39)]),
                     ),
                     child: Text(
-                      "Continue",
+                      "ถัดไป",
                       style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
                   ),
                 ),
-                SizedBox(height: 15),
+                SizedBox(height: 10),
               ],
             ),
           ),
