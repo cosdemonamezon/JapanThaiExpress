bed0e3c430f06938a82bfc79cbd68a7a3e1a0a5a
BlackBodyX64 <anonnon3952@gmail.com>
2021-06-23 01:17:11 +0700

    scan barcode

--- lib/AdminScreens/QRCodeScan/QRCodeDetail.dart
+++ lib/AdminScreens/QRCodeScan/QRCodeDetail.dart
@@ -5,6 +5,8 @@ import 'package:JapanThaiExpress/AdminScreens/WidgetsAdmin/Navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:JapanThaiExpress/utils/my_navigator.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:share/share.dart';
 
@@ -19,8 +21,31 @@ class _QRCodedetailState extends State<QRCodedetail> {
   final _screenshotController = ScreenshotController();
   static const MethodChannel _channel =
       const MethodChannel('image_gallery_saver');
-  String sendername, sendertel, senderaddress;
-  String recipname, reciptel, recipaddress;
+  String sendername = 'บริษัท เจที ขนส่ง จำกัด',
+      sendertel = '080-549-4449',
+      senderaddress = '119/8 หมู่ 4 ต.ตะพง อ.เมือง จ.ระยอง 21000';
+  String recipname = 'บริษัท อาชาเทค คอเปอเรชั่น จำกัด',
+      reciptel = '095-940-5526',
+      recipaddress =
+          '64/99 the connect 34 ถนนกาญจนาภิเษก แขวงดอกไม้ เขตประเวศ กรุงเทพมหานคร 10250';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _requestPermission();
+  }
+
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+
+    final info = statuses[Permission.storage].toString();
+    print(info);
+    // _toastInfo(info);
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -33,169 +58,171 @@ class _QRCodedetailState extends State<QRCodedetail> {
         leading: IconButton(
             icon: Icon(Icons.arrow_back),
             onPressed: () {
-              MyNavigator.goToAdmin(context);
+              Navigator.pop(context);
             }),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Screenshot(
-              controller: _screenshotController,
-              child: Card(
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                // width: 500,
-                // height: 400,
-                color: Colors.blue[50],
-
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 10, horizontal: 20),
-                                child: Text(
-                                  "ผู้ส่ง",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Screenshot(
+                controller: _screenshotController,
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                  // width: 500,
+                  // height: 400,
+                  color: Colors.blue[50],
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 20),
+                                  child: Text(
+                                    "ผู้ส่ง",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "ชื่อผู้ส่ง :${sendername}",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "ชื่อผู้ส่ง : $sendername",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "เบอร์โทรศัพท์ :${sendertel}",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "เบอร์โทรศัพท์ : $sendertel",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "ที่อยู่ :${senderaddress}",
-                                  maxLines: 3,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "ที่อยู่ : $senderaddress",
+                                    maxLines: 3,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 10, horizontal: 20),
-                                child: Text(
-                                  "ผู้รับ",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 20),
+                                  child: Text(
+                                    "ผู้รับ",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "ชื่อผู้รับ :${recipname}",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "ชื่อผู้รับ : $recipname",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "เบอร์โทรศัพท์ :${reciptel}",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "เบอร์โทรศัพท์ : $reciptel",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  "ที่อยู่ : ${recipaddress}",
-                                  maxLines: 3,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    "ที่อยู่ : $recipaddress",
+                                    maxLines: 3,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 50),
-                      ],
-                    ),
-                  ],
+                            ],
+                          ),
+                          SizedBox(height: 50),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -204,26 +231,16 @@ class _QRCodedetailState extends State<QRCodedetail> {
                 _screenshotController
                     .capture(delay: Duration(milliseconds: 10))
                     .then((capturedImage) async {
-                      saveImage(capturedImage);
-                  // ShowCapturedWidget(context, capturedImage);
+                  saveImage(capturedImage);
+                  Fluttertoast.showToast(
+                      msg: "บันทึกสำเร็จ",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.SNACKBAR,
+                      timeInSecForIosWeb: 1);
                 }).catchError((onError) {
                   print(onError);
                 });
-                // _takeScreenshot();
               },
-
-              //                     // if (_formKey1.currentState.validate()) {
-              //                     //   setState(() {
-              //                     //     isLoading = true;
-              //                     //   });
-              //                     //   _formKey1.currentState.save();
-              //                     //   _createAddmem(_formKey1.currentState.value);
-              //                     // } else {}
-              //                     // _formKey.currentState.save();
-              //                     // print(_formKey.currentState.value);
-              //                     // _preorderMem(
-              //                     //     _formKey.currentState.value);
-
               child: Container(
                 width: MediaQuery.of(context).size.width * 0.85,
                 padding: EdgeInsets.symmetric(vertical: 15),
@@ -243,7 +260,7 @@ class _QRCodedetailState extends State<QRCodedetail> {
                       colors: [Color(0xffdd4b39), Color(0xffdd4b39)]),
                 ),
                 child: Text(
-                  "บันทึก ",
+                  "บันทึก",
                   style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
               ),
@@ -269,37 +286,4 @@ class _QRCodedetailState extends State<QRCodedetail> {
     });
     return result;
   }
-
-  static Future saveFile(String file, {bool isReturnPathOfIOS = false}) async {
-    assert(file != null);
-    final result = await _channel.invokeMethod(
-        'saveFileToGallery', <String, dynamic>{
-      'file': file,
-      'isReturnPathOfIOS': isReturnPathOfIOS
-    });
-    return result;
-  }
-
-  // Future<dynamic> ShowCapturedWidget(
-  //     BuildContext context, Uint8List capturedImage) {
-  //   return showDialog(
-  //     useSafeArea: false,
-  //     context: context,
-  //     builder: (context) => Scaffold(
-  //       appBar: AppBar(
-  //         title: Text("รายละเอียด"),
-  //       ),
-  //       body: Container(
-  //           child: capturedImage != null
-  //               ? Image.memory(capturedImage)
-  //               : Container()),
-  //     ),
-  //   );
-  // }
-  // void _takeScreenshot() async{
-
-  //   final imageFile = await _screenshotController.capture();
-  //   Share.shareFiles([imageFile.path], text: "JapanThaiExpress");
-  // }
-
 }
